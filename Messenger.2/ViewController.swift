11473de7ab7d2e93cc275405dc42b8a078988f54
Heshani chamalka <63486300+Heshani-96@users.noreply.github.com>
2022-05-07 19:38:22 +0530

    Initial Commit

---
+++ Messenger.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger.2
+//
+//  Created by Heshani Chamalka on 2022-05-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
