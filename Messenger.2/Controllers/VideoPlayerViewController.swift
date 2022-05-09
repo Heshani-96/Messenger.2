ee37744398ce230b7f61be78fd2829357f8b1002
Heshani chamalka <63486300+Heshani-96@users.noreply.github.com>
2022-05-09 10:38:48 +0530

    getting started

---
+++ Messenger.2/Controllers/VideoPlayerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  VideoPlayerViewController.swift
+//  Messenger.2
+//
+//  Created by Heshani Chamalka on 2022-05-09.
+//
+
+import UIKit
+
+class VideoPlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
